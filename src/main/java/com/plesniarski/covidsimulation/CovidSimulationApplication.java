dc31b4476778dfa2012bb3c577d227b7c6047af5
dawidplesniarski <dawidplesniarski@gmail.com>
2021-01-08 16:31:00 +0100

    Initial commit

---
+++ src/main/java/com/plesniarski/covidsimulation/CovidSimulationApplication.java
@@ -0,0 +1,12 @@
+package com.plesniarski.covidsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidSimulationApplication.class, args);
+	}
+}
